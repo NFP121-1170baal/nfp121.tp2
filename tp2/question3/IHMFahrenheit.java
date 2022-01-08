9c2778fe4cc2ecccc810ed24d1292d84379b9580
Rana AL-HEK <rana.al-hek@isae.edu.lb>
2022-01-08 17:46:11 +0200

    Tp2 Done

--- tp2/question3/IHMFahrenheit.java
+++ tp2/question3/IHMFahrenheit.java
@@ -7,9 +7,7 @@ import java.awt.event.*;
 
 public class IHMFahrenheit extends JFrame implements ActionListener{
   private JTextField entree = new JTextField( 6 );
-  /** Le bouton de conversion. */
   private JButton  boutonDeConversion = new JButton( "convertir" );
-  /** La sortie en degré Celsius. */
   private JTextField sortie = new JTextField( 6 );
 
   public IHMFahrenheit(){
@@ -32,15 +30,29 @@ public class IHMFahrenheit extends JFrame implements ActionListener{
    */
   public void actionPerformed( ActionEvent ae ){
     try{
-      int fahrenheit = 0; // valeur est une String et doit être convertie en entier, voir java.lang.Integer méthode parseInt (--> try/catch)
-      float celsius = 0F; // à compléter, en appelant la méthode ad'hoc de la question2 
+      int fahrenheit = 0; 
+      float celsius = 0F;  
       // un test ici pour le zéro absolu (-273.1)
-
+      String text = entree.getText();
+      fahrenheit=java.lang.Integer.parseInt(text);
+      celsius= (float)5/9*(fahrenheit-32);
+      
       sortie.setText( Float.toString( celsius));
-    }catch(NumberFormatException nfe){
-      sortie.setText("error ! ");
-    }
-  }
+     
+      if(celsius <-273.1F)
+
+        {
+            celsius = -273.1F;
+            sortie.setText(Float.toString( celsius));
+
+        }
+        else
+        { sortie.setText( Float.toString( celsius));}
+        
+        }catch(NumberFormatException nfe){
+      sortie.setText("  error ");
+       }
+          }
   
   
   public static void main(String[] args){
